794905e3ef1307b8c01ab187240f67a01993ed45
sohee-an <soan32@hanmail.net>
2024-05-20 20:34:43 +0900

    Feat :router 구현

--- front/static/js/index.js
+++ front/static/js/index.js
@@ -1,1 +1,44 @@
-console.log("js");
+import Home from "./views/Home.js";
+
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+const router = async () => {
+  const routes = [
+    { path: "/", view: Home },
+    // { path: "/posts", view: () => console.log("viewing posts") },
+    // { path: "/settings", view: () => console.log("viewing settings") },
+  ];
+
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+
+  let match = potentialMatches.find(
+    (potentialMatche) => potentialMatche.isMatch
+  );
+  if (!match) {
+    match = {
+      route: routes[0],
+    };
+  }
+
+  const view = new match.route.view();
+  console.log("view", view);
+  document.querySelector("#root").innerHTML = await view.getHtml();
+};
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+  router();
+});
---
+++ front/static/js/views/Home.js
@@ -0,0 +1,12 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+  constructor() {
+    super();
+    this.setTitle("Home");
+  }
+  async getHtml() {
+    return `
+    <h1>hihihi</h1>`;
+  }
+}
