a5178365e40494365760d0b25f613d051ff793c1
sohee-an <soan32@hanmail.net>
2024-05-22 17:52:42 +0900

    Feat: 라우터 파라미터 받는 부분 구현

--- front/static/js/views/PostView.js
+++ front/static/js/views/PostView.js
@@ -3,10 +3,11 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
   constructor(params) {
     super(params);
-    this.setTitle("Setting");
+    this.setTitle("Post");
   }
   async getHtml() {
+    console.log("hihi", this.params.id);
     return `
-    <h1>Welcome Setting</h1>`;
+    <h1>Welcome Posts ${this.params.id}</h1>`;
   }
 }
